f2e6dad1582bb5d06c7665f57f64929a8125c115
isralr <ilaramac@myuax.com>
2024-10-29 11:10:02 +0100

    Ejercicio examen completado

--- main.cpp
+++ main.cpp
@@ -1,3 +1,18 @@
-//
-// Created by israe on 29/10/2024.
-//
+#include <iostream>
+#include <cmath>
+
+int main() {
+    std::setlocale(LC_ALL, "");
+    std::wcout.imbue(std::locale(""));
+
+    std::wcout << L"Triángulos pitagóricos:" << std::endl;
+    for (int a = 1; a <= 20; a++) {
+        for (int b = a; b <=20; b++) {
+            int c = static_cast<int>(sqrt(pow(a,2) + pow (b,2)));
+            if (pow(c,2) == pow(a,2) + pow(b,2)) {
+                std::cout << "-" << a << "-" << b << "-" << c << std::endl;
+            }
+        }
+    }
+    return 0;
+}
